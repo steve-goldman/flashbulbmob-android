fe733a12da0595ea544d174a503b5ef00917c7fa
Steve Goldman <steve.goldman@gmail.com>
2016-03-09 14:12:31 -0500

    toggle torch with floating action button

--- app/src/main/java/constantbeta/com/flashbulbmob/MyActivity.java
+++ app/src/main/java/constantbeta/com/flashbulbmob/MyActivity.java
@@ -1,11 +1,15 @@
 package constantbeta.com.flashbulbmob;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -14,6 +18,9 @@ import android.widget.EditText;
 public class MyActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.constantbeta.flashbulbmob.MESSAGE";
 
+    private Camera camera;
+    private boolean isFlashOn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,12 +28,37 @@ public class MyActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        final boolean hasFlash = getApplicationContext().getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+
+        if (!hasFlash) {
+            AlertDialog alert = new AlertDialog.Builder(MyActivity.this)
+                    .setTitle("Error")
+                    .setMessage("Device does not support flash")
+                    .setCancelable(false)
+                    .setNegativeButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            finish();
+                        }
+                    })
+                    .create();
+            alert.show();
+            return;
+        }
+
+        getCamera();
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own message", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if (isFlashOn) {
+                    turnOffFlash();
+                }
+                else {
+                    turnOnFlash();
+                }
             }
         });
     }
@@ -53,11 +85,50 @@ public class MyActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onDestroy() {
+        if (camera != null) {
+            camera.release();
+        }
+        super.onDestroy();
+    }
+
     public void sendMessage(View view) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText)findViewById(R.id.edit_message);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    private void getCamera() {
+        if (camera == null) {
+            try {
+                camera = Camera.open();
+            }
+            catch (RuntimeException e) {
+                Log.e("Camera error: ", e.getMessage());
+            }
+        }
+    }
+
+    private void turnOnFlash() {
+        if (camera != null && !isFlashOn) {
+            Camera.Parameters params = camera.getParameters();
+            params.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+            camera.setParameters(params);
+            camera.startPreview();
+            isFlashOn = true;
+        }
+    }
+
+    private void turnOffFlash() {
+        if (camera != null && isFlashOn) {
+            Camera.Parameters params = camera.getParameters();
+            params.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+            camera.setParameters(params);
+            camera.stopPreview();
+            isFlashOn = false;
+        }
     }
 }
