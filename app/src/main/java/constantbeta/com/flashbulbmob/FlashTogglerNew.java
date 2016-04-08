ebab6a192f0fd8712f856472869d808af664005c
bigjosh <github@junk.josh.com>
2016-04-08 10:23:34 -0400

    Make camera exceptions percolate
    Changes to move flashtoggler constructor into main thread by doing it in
    start button. The lets exceptions percolate and become visible rather
    than getting consumed in an empty try/catch.

--- app/src/main/java/constantbeta/com/flashbulbmob/FlashTogglerNew.java
+++ app/src/main/java/constantbeta/com/flashbulbmob/FlashTogglerNew.java
@@ -1,6 +1,7 @@
 package constantbeta.com.flashbulbmob;
 
 import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
 import android.os.Build;
@@ -13,7 +14,7 @@ public class FlashTogglerNew implements FlashToggler {
     private final String cameraId;
     private boolean isFlashOn;
 
-    public FlashTogglerNew(Context context) throws Exception {
+    public FlashTogglerNew(Context context) throws CameraAccessException {
         manager = (CameraManager) context.getSystemService(Context.CAMERA_SERVICE);
         cameraId = findCamera();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -76,12 +77,15 @@ public class FlashTogglerNew implements FlashToggler {
         }
     }
 
-    private String findCamera() throws Exception {
+    // Looks for any camera with an associated flash device. Returns 1st one found or null if none
+    private String findCamera() throws CameraAccessException {
         for (final String cameraId : manager.getCameraIdList()) {
             if (manager.getCameraCharacteristics(cameraId).get(CameraCharacteristics.FLASH_INFO_AVAILABLE)) {
                 return cameraId;
             }
         }
-        throw new Exception("Could not find a camera that has a flash");
+
+        return null;    // No camera with flash found
+
     }
 }
